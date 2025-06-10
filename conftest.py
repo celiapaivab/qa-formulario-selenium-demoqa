93f406e47530606ab6747916ae11d5a790b9a963
celiapaivab <celiadepaivabruno@gmail.com>
2025-06-10 19:31:28 -0300

    Conftest modified to headless

--- conftest.py
+++ conftest.py
@@ -1,11 +1,18 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.maximize_window()
+    options = Options()
+    options.add_argument('--headless')  # Executa sem interface gráfica
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.set_window_size(1920, 1080)
     yield driver
     driver.quit()
